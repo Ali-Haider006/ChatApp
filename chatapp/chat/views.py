c82149754f343bfed44d3f7d12931363f6ff01b5
Ali Haider <ah0062527@gmail.com>
2024-10-08 11:59:38 +0500

    Starting project again

--- chatapp/chat/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import render
-from .models import Message
-
-def chat_view(request):
-    messages = Message.objects.all().order_by('timestamp')
-    return render(request, 'chat/chat.html', {'messages': messages})
-
-# Create your views here.
